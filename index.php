83053a9f3b450b39044db4ae590ead96c23fc8f5
Matias S <matias.sotomayor@gmail.com>
2017-09-08 22:40:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2016 Matías Sotomayor Ramos
+ * Copyright (C) 2017 Matías Sotomayor Ramos
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
